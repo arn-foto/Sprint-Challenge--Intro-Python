20d24679fdd10f5352064cb336c8584211828c54
Anthony N <foto.arn@gmail.com>
2020-09-04 09:18:16 -0700

    + Class hierarchy to 'oop.py'

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -18,10 +18,10 @@
 #
 # Put a comment noting which class is the base class
 #
-#### This will be the base ####
+#
 class Vehicle():
     pass
-
+#### This will be the base class ####
 class FlightVehicle(Vehicle):
     pass
 
